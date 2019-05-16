bfc2b0cd7c252f46301c519a0f0f0b41c6494f53
iris paredes <irisparedesjimenez@gmail.com>
2019-05-16 11:24:51 +0200

    add palette colors in preview

--- src/components/MainCard.js
+++ src/components/MainCard.js
@@ -3,40 +3,13 @@ import Form from './Form';
 import Preview from './Preview';
 
 class MainCard extends React.Component{
-    constructor(props) {
-        super(props);
-        this.state = {
-            name:'',
-            job:'',
-            email:'',
-            tel:'',
-            linkedin:'',
-            github:'',
-            palette: 1,
-        }
-        this.handleInput = this.handleInput.bind(this);
-        this.handleColor = this.handleColor.bind(this);
-    }
-    handleColor(event){
-        const value = event.currentTarget.value;
-        this.setState({palette: value});
-    }
-    handleInput(event) {
-        const currentField = event.currentTarget;
-        const key = currentField.id;
-        const value = currentField.value;
-        const newValue = {};
-        newValue[key] = value;
-
-        console.log(newValue);
-        this.setState(newValue);
-    }
     render(){
-        const {name, job, email, tel, linkedin, github, palette}=this.state;
+        const {url, state, handleColor, handleInput} = this.props;
+        const {palette, name, job, email, tel, linkedin, github} = state;
         return(
             <main className="main__container">
-            <Form actionToPerform={this.handleInput} actionColor={this.handleColor} palette={palette}/>
-            <Preview url={this.props.url} name={name} job={job} email={email} tel={tel} linkedin={linkedin} github={github}/>
+            <Form actionToPerform={handleInput} actionColor={handleColor} palette={palette}/>
+            <Preview url={url} name={name} job={job} email={email} tel={tel} linkedin={linkedin} github={github} palette={palette}/>
             </main>
         );
     }
