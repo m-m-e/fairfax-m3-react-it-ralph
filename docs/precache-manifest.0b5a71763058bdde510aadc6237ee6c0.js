2d76184078b2cae9781604828e3eb5dc944b2973
Marta Sobrino <marta.sobrino.web@gmail.com>
2019-05-22 19:58:01 +0200

    build docs folder

---
+++ docs/precache-manifest.0b5a71763058bdde510aadc6237ee6c0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8cfa6ef0392cc95bbcc6bf2ddd5a266",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7394add3328feb1e0bd6",
+    "url": "./static/css/main.f199a349.chunk.css"
+  },
+  {
+    "revision": "a42cdb4e6396353f81fd",
+    "url": "./static/js/2.6bfcf457.chunk.js"
+  },
+  {
+    "revision": "7394add3328feb1e0bd6",
+    "url": "./static/js/main.bae33e48.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "8b3e9cb639bb75dc4530bd7aa8dd88b0",
+    "url": "./static/media/jigsaw_logo.8b3e9cb6.svg"
+  }
+]);
