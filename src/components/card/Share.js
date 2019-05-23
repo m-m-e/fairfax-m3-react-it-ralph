6344391669f1627935cdbaa1030c0ab766f2d429
Marta Sobrino <marta.sobrino.web@gmail.com>
2019-05-23 17:38:34 +0200

    no comments

--- src/components/card/Share.js
+++ src/components/card/Share.js
@@ -3,11 +3,11 @@ import React from 'react';
 class Share extends React.Component{
     render (){
         return(
-                <div className="container__button collapsible__container">
-                    <button className="share__button" type="submit">
-                        <i className="far fa-address-card share__icon"></i>Crear tarjeta
-                    </button>
-                </div>            
+            <div className="container__button collapsible__container">
+                <button className="share__button" type="submit">
+                    <i className="far fa-address-card share__icon"></i>Crear tarjeta
+                </button>
+            </div>            
         );
     }
 }
